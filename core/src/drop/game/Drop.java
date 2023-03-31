449f3c01cf24458f573b17f78c6e77b6ee67591b
IgaJ <i.jarzewicz@gmail.com>
2023-03-31 13:57:48 +0200

    simple game extended (Screen interface, Game class)

---
+++ core/src/drop/game/Drop.java
@@ -0,0 +1,20 @@
+package drop.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+public class Drop extends Game {
+
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+
+    }
+}
